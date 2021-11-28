5f7e42031d9bd71cdc7dec274045c9ecbacba139
jmunkc <jona04g8@stud.kea.dk>
2021-11-28 20:16:52 +0100

    Project initialization

---
+++ src/main/java/com/example/plantool/PlantoolApplication.java
@@ -0,0 +1,13 @@
+package com.example.plantool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlantoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/plantool/PlantoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.plantool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
