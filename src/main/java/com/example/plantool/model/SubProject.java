9326fef56837e4ab06f8aad56690ff31cdb78212
jmunkc <jona04g8@stud.kea.dk>
2021-11-30 11:17:26 +0100

    Created Project class with subClasses

---
+++ src/main/java/com/example/plantool/model/SubProject.java
@@ -0,0 +1,11 @@
+package com.example.plantool.model;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class SubProject extends Project {
+
+    SubProject(int id, String name, Date startDate, Date endDate, Date deadline, ArrayList<ProjectMember> assignees, int hoursAllocated, int hoursUsed, ArrayList<skill> skillsAllocated) {
+        super(id, name, startDate, endDate, deadline, assignees, hoursAllocated, hoursUsed, skillsAllocated);
+    }
+}
