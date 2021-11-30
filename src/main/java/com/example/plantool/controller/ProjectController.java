c2151dae86e2f1b6828deead4a70951cf29261e6
Lars <larsbrogaard86@gmail.com>
2021-11-30 13:09:21 +0100

    Oprettet klasser: ProjectController/Service/Repo

--- src/main/java/com/example/plantool/controller/ProjectController.java
+++ src/main/java/com/example/plantool/controller/ProjectController.java
@@ -1,4 +1,8 @@
 package com.example.plantool.controller;
 
+import com.example.plantool.model.Project;
+
 public class ProjectController {
+
+
 }
--- src/main/java/com/example/plantool/repository/ProjectRepository.java
+++ src/main/java/com/example/plantool/repository/ProjectRepository.java
@@ -1,5 +1,13 @@
 package com.example.plantool.repository;
 
+import com.example.plantool.model.Project;
+import com.example.plantool.utility.DatabaseConnector;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class ProjectRepository {
 
 
@@ -8,12 +16,36 @@ public class ProjectRepository {
 
     }
 
-    public Project fetchSingleProject(int projectId){
 
-        return project;
+    public Project fetchSingleProject(int projectId)  {
+        Project tmp = new Project(0,null,null,null,null,null,0,0,null);
+
+        try {
+            PreparedStatement stmt = DatabaseConnector.getConnection().prepareStatement
+                    ("SELECT projectname, projectstartdate, projectenddate, projectdeadline FROM project WERE projectid=" + projectId + "");
+
+            ResultSet resultSet = stmt.executeQuery();
+
+            while(resultSet.next()){
+                tmp.setId(resultSet.getInt(1));
+                tmp.setName(resultSet.getString(2));
+                tmp.setStartDate(resultSet.getDate(3));
+                tmp.setEndDate(resultSet.getDate(4));
+                tmp.setDeadline(resultSet.getDate(5));
+            }
+            return tmp;
+
+
+        } catch (SQLException e){
+            System.out.println();
+        }
+
+        return tmp;
+
     }
 
     public ArrayList<Project> fetchMultipleProjects(){
+        return null;
 
     }
 
