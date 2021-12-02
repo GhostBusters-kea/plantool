251a6fe22cbc64214be53eaf609d316450b27fab
jmunkc <jona04g8@stud.kea.dk>
2021-12-02 10:15:30 +0100

    Update attibutes of User and Project

--- src/main/java/com/example/plantool/model/SubProject.java
+++ src/main/java/com/example/plantool/model/SubProject.java
@@ -5,7 +5,7 @@ import java.util.Date;
 
 public class SubProject extends Project {
 
-    SubProject(int id, String name, Date startDate, Date endDate, Date deadline, ArrayList<ProjectMember> assignees, int hoursAllocated, int hoursUsed, ArrayList<Skill> skillsAllocated) {
-        super(id, name, startDate, endDate, deadline, assignees, hoursAllocated, hoursUsed, skillsAllocated);
+    SubProject(String name, Date startDate, Date endDate, Date deadline, ArrayList<ProjectMember> assignees, int hoursAllocated, int hoursUsed, ArrayList<String> skillsAllocated) {
+        super(name, startDate, endDate, deadline, assignees, hoursAllocated, hoursUsed, skillsAllocated);
     }
 }
--- src/main/java/com/example/plantool/model/SubTask.java
+++ src/main/java/com/example/plantool/model/SubTask.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class SubTask extends Project{
-    SubTask(int id, String name, Date startDate, Date endDate, Date deadline, ArrayList<ProjectMember> assignees, int hoursAllocated, int hoursUsed, ArrayList<Skill> skillsAllocated) {
-        super(id, name, startDate, endDate, deadline, assignees, hoursAllocated, hoursUsed, skillsAllocated);
+    SubTask(String name, Date startDate, Date endDate, Date deadline, ArrayList<ProjectMember> assignees, int hoursAllocated, int hoursUsed, ArrayList<String> skillsAllocated) {
+        super(name, startDate, endDate, deadline, assignees, hoursAllocated, hoursUsed, skillsAllocated);
     }
 }
