1e0779a042a5e778ce0a6f8b1a0d1caff1c6dc23
MichaelDyvad <michaeldyvad@gmail.com>
2021-12-13 21:19:09 +0100

    subprojectController

--- src/main/java/com/example/plantool/controller/TaskController.java
+++ src/main/java/com/example/plantool/controller/TaskController.java
@@ -6,8 +6,11 @@ import com.example.plantool.services.TaskService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.context.request.WebRequest;
 
 import javax.servlet.http.HttpSession;
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 @Controller
@@ -16,12 +19,14 @@ public class TaskController {
     TaskService taskService = new TaskService();
 
     @GetMapping("/viewtask")
-    public String taskView(Model model, HttpSession session){
+    public String taskView(Model model, HttpSession session, WebRequest wr){
         int memberLead = (Integer) session.getAttribute("boolean-leader");
         String userId = (String) session.getAttribute("userid");
 
+        //int subprojectId = Integer.parseInt(wr.getParameter("subproject-id"));
+        int subprojectId = (Integer) model.getAttribute("subproject-id");
 
-        ArrayList<Task> taskArray = taskService.fetchAllTasks(1);
+        ArrayList<Task> taskArray = taskService.fetchAllTasks(subprojectId);
 
         model.addAttribute("tasks", taskArray);
 
@@ -31,5 +36,27 @@ public class TaskController {
             return "viewtaskformember";
 
     }
+
+    @GetMapping("/createtask")
+    public String createTaskGet(HttpSession session){
+        return "createtask";
+    }
+
+    @PostMapping("/createtask")
+    public String createTaskPost(HttpSession session, WebRequest wr){
+        String name = wr.getParameter("taskName");
+        LocalDate startDate = LocalDate.parse(wr.getParameter("taskStartDate"));
+        LocalDate deadline = LocalDate.parse(wr.getParameter("taskDeadline"));
+        int hoursAllocated = Integer.parseInt(wr.getParameter("taskHoursAllocated"));
+        String projectDescription = wr.getParameter("taskDescription");
+
+        Task task = new Task(name, startDate, deadline, deadline, hoursAllocated, projectDescription);
+        taskService.addTaskToDb(task, 1); //TODO subproject skal fixes
+
+
+        return "redirect:/viewtask";
+    }
+
+
 
 }
