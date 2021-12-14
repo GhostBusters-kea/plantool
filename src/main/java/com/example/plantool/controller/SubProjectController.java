c14095b94cf1895c498defd3ebbc0d9499392ea4
Lars <larsbrogaard86@gmail.com>
2021-12-14 09:27:44 +0100

    update i subtask done

---
+++ src/main/java/com/example/plantool/controller/SubProjectController.java
@@ -0,0 +1,32 @@
+package com.example.plantool.controller;
+
+import com.example.plantool.model.SubProject;
+import com.example.plantool.services.SubProjectService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+
+@Controller
+public class SubProjectController {
+    SubProjectService subProjectService = new SubProjectService();
+
+    @GetMapping("/viewsubproject")
+    public String subprojectView(Model model, HttpSession session) {
+        int memberLead = (Integer) session.getAttribute("boolean-leader");
+        int projectId = (Integer) session.getAttribute("projectId");
+        System.out.println(projectId);
+
+        ArrayList<SubProject> subProjectArray = subProjectService.fetchAllSubProjectsFromProject(1);
+
+        model.addAttribute("subprojects", subProjectArray);
+
+
+        if (memberLead == 1) {
+            return "viewsubprojectleader";
+        } else
+            return "viewsubprojectmember";
+    }
+}
