8789a9767947d9da725d618e701be92f3aa2f73d
KevinFHansen <kevi4777@stud.kea.dk>
2021-12-14 15:02:20 +0100

    Alle link skulle fungere

--- src/main/java/com/example/plantool/controller/SubProjectController.java
+++ src/main/java/com/example/plantool/controller/SubProjectController.java
@@ -1,25 +1,65 @@
 package com.example.plantool.controller;
 
 import com.example.plantool.model.SubProject;
+import com.example.plantool.services.MemberService;
 import com.example.plantool.services.SubProjectService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.context.request.WebRequest;
 
 import javax.servlet.http.HttpSession;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 @Controller
 public class SubProjectController {
     SubProjectService subProjectService = new SubProjectService();
+    MemberService memberService = new MemberService();
 
-    @GetMapping("/viewsubproject")
+
+   @GetMapping("/viewsubproject")
+    public String subProjectOverview(Model model, HttpSession session) throws SQLException {
+
+       int projectId = (Integer) session.getAttribute("projectId");
+       int memberLead = (Integer) session.getAttribute("boolean-leader");
+
+        if(memberLead == 1){
+            String mapping = memberService.inSession(model, session, "viewsubprojectleader");
+            ArrayList<SubProject> subProjects = subProjectService.fetchAllSubProjectsFromProject(projectId);
+            model.addAttribute("subprojects", subProjects);
+            return mapping;
+        }
+
+        else {
+            String mapping = memberService.inSession(model, session, "viewsubprojectmember");
+            ArrayList<SubProject> subProjects = subProjectService.fetchAllSubProjectsFromProject(projectId);
+            model.addAttribute("subprojects", subProjects);
+            return mapping;
+        }
+
+    }
+    @PostMapping("/viewsubproject")
+    public String postSubProject(WebRequest wr, HttpSession session) throws SQLException{
+        int leaderId = Integer.parseInt(session.getAttribute("userid").toString());
+        int subProjectId = Integer.parseInt(wr.getParameter("subprojectId"));
+        session.setAttribute("subprojectId", subProjectId);
+        return "redirect:/viewtask";
+    }
+
+
+
+
+
+/*
+   @GetMapping("/viewsubproject")
     public String subprojectView(Model model, HttpSession session) {
         int memberLead = (Integer) session.getAttribute("boolean-leader");
         int projectId = (Integer) session.getAttribute("projectId");
         System.out.println(projectId);
 
-        ArrayList<SubProject> subProjectArray = subProjectService.fetchAllSubProjectsFromProject(1);
+        ArrayList<SubProject> subProjectArray = subProjectService.fetchAllSubProjectsFromProject(projectId);
 
         model.addAttribute("subprojects", subProjectArray);
 
@@ -29,4 +69,8 @@ public class SubProjectController {
         } else
             return "viewsubprojectmember";
     }
+
+ */
+
+
 }
--- src/main/java/com/example/plantool/controller/SubTaskController.java
+++ src/main/java/com/example/plantool/controller/SubTaskController.java
@@ -2,6 +2,7 @@ package com.example.plantool.controller;
 
 
 import com.example.plantool.model.SubTask;
+import com.example.plantool.model.Task;
 import com.example.plantool.services.MemberService;
 import com.example.plantool.services.SubTaskService;
 import org.springframework.stereotype.Controller;
@@ -9,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpSession;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 @Controller
@@ -17,18 +19,21 @@ public class SubTaskController {
     SubTaskService subTaskService = new SubTaskService();
 
     @GetMapping("/viewsubtask")
-    public String subTaskView(Model model, HttpSession session){
-        int memberlead = (Integer) session.getAttribute("boolean-leader");
-        String userId = (String) session.getAttribute("userid");
+    public String viewSubTask (Model model, HttpSession session) throws SQLException {
+        int taskId = (Integer) session.getAttribute("taskId");
+        int memberLead = (Integer) session.getAttribute("boolean-leader");
 
-        ArrayList<SubTask> subTaskArrayList = subTaskService.fetchAllSubTask(1);
-        model.addAttribute("subtasks", subTaskArrayList);
-
-        if (memberlead == 1){
-            return "viewsubtaskforleader";
+        if (memberLead ==1){
+            String mapping = memberService.inSession(model, session, "viewsubtaskforleader");
+            ArrayList<SubTask> subTasks = subTaskService.fetchAllSubTask(taskId);
+            model.addAttribute("subtasks", subTasks);
+            return mapping;
         }
-        else{
-            return "viewsubtaskformember";
+        else {
+            String mapping = memberService.inSession(model, session, "viewsubtaskformember");
+            ArrayList<SubTask> subtasks = subTaskService.fetchAllSubTask(taskId);
+            model.addAttribute("subtasks", subtasks);
+            return mapping;
         }
     }
 }
